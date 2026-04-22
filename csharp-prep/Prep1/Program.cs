4024a43385b0ad17277ec88aec4acd1f4bf570cf
aba-o <otchereaba@byui.edu>
2026-04-22 13:05:10 -0600

    Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.Write("Hello user! What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
+// I would've loved to put a capitalize function where it displavs a capitalized version of the names, 
+// no matter how it was typed
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,69 @@
 using System;
+using System.Net.Security;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Hello Student! Enter your grade percentage ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string letterGrade = "";
+
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+        
+        int lastDigit = grade % 10;
+        string sign = "";
+
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else 
+        {
+            sign = "-";
+        }
+
+        if (letterGrade == "A" && sign == "+")
+        {
+            sign = "";
+        }
+        if (letterGrade == "F")
+        {
+            sign = "";
+        }
+
+
+        Console.WriteLine($"Your letter grade is {letterGrade}{sign}!");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Yayy! You passed this class!");
+        }
+        else
+        {
+            Console.WriteLine("Bummer, you failed!");
+        }
+
     }
 }
